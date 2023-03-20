f51079d0ec85f80127fbd5979f5ee3963f2a88b4
Pawel <71873238+pawelgoj@users.noreply.github.com>
2023-03-20 23:10:32 +0100

    Add models

--- beauty_services_website/business/admin.py
+++ beauty_services_website/business/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Service, Contact, SocialMedia, Clients
 
 # Register your models here.
+# możemy dodaćkolumny które widzi administrator 
+#dekorator do rejstrowania danej tabeli 
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    
+@admin.register(SocialMedia)
+class SocialMediaAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    
+@admin.register(Clients)
+class ClientsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
--- beauty_services_website/business/models.py
+++ beauty_services_website/business/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Service(models.Model):
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    name = models.CharField(max_length=100)
+    short_description = models.CharField(max_length=200)
+    long_description = models.TextField()
+    
+    class Meta:
+        ordering =('name',)
+
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=15, blank=True)
+    question = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class SocialMedia(models.Model):
+    name = models.CharField(max_length=100)
+    link = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Clients(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100, blank=True)
+    phone = models.CharField(max_length=15, blank=True)
+    email = models.EmailField(max_length=100, blank=True)
+    description = models.CharField(max_length=500, blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
